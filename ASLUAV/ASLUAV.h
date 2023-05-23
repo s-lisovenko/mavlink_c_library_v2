277bf5424b7ef8c4c4785f0c5872e53da949dff6
Sergii Lisovenko <2522054+s-lisovenko@users.noreply.github.com>
2023-05-23 17:22:10 +0300

    Update mavlink library

--- ASLUAV/ASLUAV.h
+++ ASLUAV/ASLUAV.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ASLUAV.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ASLUAV_XML_HASH 16191895962269560
+#define MAVLINK_ASLUAV_XML_HASH 3243647466343189693
 
 #ifdef __cplusplus
 extern "C" {
--- minimal/minimal.h
+++ minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH 5667433024371497101
+#define MAVLINK_MINIMAL_XML_HASH -7223706884524185040
 
 #ifdef __cplusplus
 extern "C" {
