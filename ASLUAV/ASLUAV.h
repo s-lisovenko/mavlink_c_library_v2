210cdf9360d06be5ba25848be1f388639af75ab2
Sergii Lisovenko <2522054+s-lisovenko@users.noreply.github.com>
2023-06-24 11:44:33 +0300

    Update mavlink library

--- ASLUAV/ASLUAV.h
+++ ASLUAV/ASLUAV.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ASLUAV.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ASLUAV_XML_HASH -4712230565124398393
+#define MAVLINK_ASLUAV_XML_HASH -5111909313538897557
 
 #ifdef __cplusplus
 extern "C" {
--- csAirLink/csAirLink.h
+++ csAirLink/csAirLink.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_CSAIRLINK.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_CSAIRLINK_XML_HASH 9192059681117426193
+#define MAVLINK_CSAIRLINK_XML_HASH -2952195134153949865
 
 #ifdef __cplusplus
 extern "C" {
